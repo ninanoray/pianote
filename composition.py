4c6b291ab6877c13e1e6e2c86a13bb19dd63ca20
ninanoray <77103814+ninanoray@users.noreply.github.com>
2022-09-23 13:23:37 +0900

    upload sound file, note example

--- composition.py
+++ composition.py
@@ -1,16 +1,32 @@
-from pygame import mixer
 import vlc
 import time
 
-c4 = "sound\main\C4.mp3"
-d4 = "sound\main\D4.mp3"
-e4 = "sound\main\E4.mp3"
+class Pitch:
+    def __int__(self, name, len):
+        self.name = name
+        self.len = len
+        self.file = ""
 
-def play(fileList, timeLen) :
-    for file in fileList:
-        sound = vlc.MediaPlayer(file)
+    def play(self):
+        sound = vlc.MediaPlayer(self.file)
         sound.play()
-    time.sleep(timeLen)
+        time.sleep(self.len)
 
-play([c4], 1)
-play([e4], 1)
+class C(Pitch):
+    def __int__(self, name, len):
+        super().__int__(name, len)
+        self.name = name
+        self.c4 = "sound\main\C4.mp3"
+        self.c5 = "sound\main\C5.mp3"
+        self.file = self.match_file()
+
+    def match_file(self):
+        if self.name == "c4":
+            return self.c4
+        elif self.name == "c5":
+            return self.c5
+
+
+if __name__ == '__main__':
+    c4 = Pitch('c4', 1)
+
